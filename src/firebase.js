917e04b8f0f68cd323295d0e2d13fc30b15f6862
Aakash Sharma <sharmaaakash824@gmail.com>
2022-05-14 12:01:25 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBz-bzhImV2YNbms1vsZy6BpShf6uL95vg",
+    authDomain: "netflix-2ec4d.firebaseapp.com",
+    projectId: "netflix-2ec4d",
+    storageBucket: "netflix-2ec4d.appspot.com",
+    messagingSenderId: "181020917217",
+    appId: "1:181020917217:web:bcb7f5208c83bbc72ce6a0"
+};
