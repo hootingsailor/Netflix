770844ddb2cec0cb09ed983585f26db2048645d2
Aakash Sharma <sharmaaakash824@gmail.com>
2022-05-17 21:44:28 +0530

    app deployed and start working on signin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './Components/HomeScreen';
+import React from "react";
+import "./App.css";
+import HomeScreen from "./Components/HomeScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
